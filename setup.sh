82e3a1b0973871f3d7483908701ee56a223ca6dd
ihounie <ihounie@fing.edu.uy>
2020-09-22 17:40:40 -0300

    cuchufli

--- setup.sh
+++ setup.sh
@@ -8,17 +8,17 @@ export SONYC_UST_PATH=~/sonyc-ust
 yes | pip3 install -r requirements.txt
 pip3 install comet_ml
 # Download VGGish model files
-mkdir -p ~/vggish
-pushd ~/vggish
-curl -O https://storage.googleapis.com/audioset/vggish_model.ckpt
-curl -O https://storage.googleapis.com/audioset/vggish_pca_params.npz
-popd
+#mkdir -p ../vggish
+#pushd ../vggish
+#curl -O https://storage.googleapis.com/audioset/vggish_model.ckpt
+#curl -O https://storage.googleapis.com/audioset/vggish_pca_params.npz
+#popd
 
 # Download dataset
 #mkdir -p ~/data
 #pushd ~/data
 #wget https://zenodo.org/record/2590742/files/annotations.csv
-#wget https://zenodo.org/record/2590742/files/audio.tar.gz
+wget https://zenodo.org/record/2590742/files/audio.tar.gz
 #wget https://zenodo.org/record/2590742/files/dcase-ust-taxonomy.yaml
 #wget https://zenodo.org/record/2590742/files/README.md
 
